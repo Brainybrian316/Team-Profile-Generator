f4559f00cdb74645803f6a37b2064c7b7f7bfae3
Brainybrian316 <brainybrian316@gmail.com>
2022-04-23 11:25:42 -0400

    editing html styling

--- src/page-template.js
+++ src/page-template.js
@@ -16,29 +16,32 @@ function generateHTML(data) {
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
             integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
             <title>Team Profile Generator</title>
             </head>
             
             <body>
+            <h1 class="text-center bg-primary text-white">My Team</h1>
             <div class="container">
             <div class="row">
             <div class="col-10 mx-auto">
-            <h1 class="text-center">My Team</h1>
             <div class="card-group">
             <div class="card">
             <div class="card-body">
             <h5 class="card-title">${data.name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${data.title}</h6>
-            <p class="card-text">${data.id}</p>
-            <p class="card-text">${data.email}</p>
+            <p class="card-text">ID: ${data.id}</p>
+            <p class="card-text">Email: ${data.email}</p>
             </div>
             </div>
             </div>
             </div>
             </div>
             </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+            </script>
             </body>
             `
 }
