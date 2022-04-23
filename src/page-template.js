5f35b4f838fc28b57634e3490f3c0d7e74290830
Brainybrian316 <brainybrian316@gmail.com>
2022-04-23 12:19:02 -0400

    edited generatehtml

--- src/page-template.js
+++ src/page-template.js
@@ -18,23 +18,20 @@ function generateHTML(data) {
             integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <script src="https://kit.fontawesome.com/0c923aefe5.js" crossorigin="anonymous"></script>
             <title>Team Profile Generator</title>
             </head>
             
             <body>
             <h1 class="text-center bg-primary text-white">My Team</h1>
-            <div class="container">
-            <div class="row">
-            <div class="col-10 mx-auto">
-            <div class="card-group">
-            <div class="card">
+            <div class="row d-flex justify-content-evenly p-5 mx-auto">
+            <div class="col-3">
+            <div class="card bg-info">
             <div class="card-body">
-            <h5 class="card-title">${data.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${data.title}</h6>
-            <p class="card-text">ID: ${data.id}</p>
-            <p class="card-text">Email: ${data.email}</p>
-            </div>
-            </div>
+            <h5 class="card-title fs-1">${data.name}</h5>
+            <h6 class="card-subtitle mb-2  fs-3 text-muted border-bottom border-2">${data.title}</h6>
+            <p class="card-text p-1 fs-5">ID: ${data.id}</p>
+            <p class="card-text fs-5">Email: ${data.email}</p>
             </div>
             </div>
             </div>
