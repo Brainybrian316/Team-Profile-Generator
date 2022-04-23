f3f11b4817fa50283c5e81a4a9bc24b30922f124
Brainybrian316 <brainybrian316@gmail.com>
2022-04-23 14:49:22 -0400

    all test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,7 +3,7 @@ const Employee = require('../lib/Employee');
 test('creates an employee object', () => {
     const employee = new Employee('John', 1, 'email');
 
-    expect(employee.name).toBe('John');
+    expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 
